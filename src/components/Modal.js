729db2c7d558c8e33d9f43b6d0df59062fd70ab5
ohick <oliviahick1@gmail.com>
2020-11-19 15:14:32 +0100

    CRUD completed

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 
 const Modal = (props) => {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">
-        <div className="header">Delete Stream</div>
-        <div className="content">
-          Are you sure you want to delete this stream?
-        </div>
-        <div className="actions">
-          <button className="ui primary button">Delete</button>
-          <button className="ui button">Cancel</button>
-        </div>
+    <div onClick={props.onDismiss} className="ui dimmer modals visible active">
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="ui standard modal visible active">
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
       </div>
     </div>,
     document.querySelector('#modal')
